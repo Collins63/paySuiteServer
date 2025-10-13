d32f28a987201a9c707947d54580a35aa29d86ae
MadScientist <kudzaiseanchiriseri1@gmail.comt>
2025-10-13 10:04:10 +0200

    Full MiddleWare

--- paySuite/paySuite/settings.py
+++ paySuite/paySuite/settings.py
@@ -60,6 +60,8 @@ MIDDLEWARE = [
    
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
